32aee03e3914fcb16c885c1872eefd320ad6e44f
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2025-01-08 21:24:45 +0200

    firebase

---
+++ firebase/index.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+
+const firebaseConfig = {
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log(app);
